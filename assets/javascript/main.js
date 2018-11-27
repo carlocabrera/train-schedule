49dec5cbcbee2ef093fa7c88eea5b167569423ab
Carlo Cabrera <41243450+carlocabrera@users.noreply.github.com>
2018-11-26 21:38:07 -0500

    js pseudocode

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+// Global Variables
+// Initialize Firebase
+// Firebase Variables
+// Now and First Train Time Difference
+// Remainder of Time
+// Subtract Remainder From Frequency
+// Next Train
+// Display Train Info
+// Input Function
+// Input Values
+// Firebase Database
+// Alert Added Train and Clear Form
+// Call Functions
